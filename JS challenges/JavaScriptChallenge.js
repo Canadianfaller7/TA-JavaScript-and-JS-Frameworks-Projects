9a85ccff5f8f3b537eba93c4f9a8706da3f00502
Spencer Merrill <canadianfaller7@gmail.com>
2022-03-16 15:57:30 -0600

    Uncommented my code

--- JS challenges/JavaScriptChallenge.js
+++ JS challenges/JavaScriptChallenge.js
@@ -1,68 +1,68 @@
-// // bitwise &: Sets each bit to 1 if both bits are 1
-// let x = 5 & 1;
-// console.log(x);
+// bitwise &: Sets each bit to 1 if both bits are 1
+let x = 5 & 1;
+console.log(x);
 
-// // bitwise OR: Sets each bit to 1 if one of two bits is 1
-// let y = 5 | 1;
-// console.log(y);
-// // bitwise XOR: Sets each bit to 1 if only one of two bits is 1
-// let z = 5 ^ 1;
-// console.log(z);
+// bitwise OR: Sets each bit to 1 if one of two bits is 1
+let y = 5 | 1;
+console.log(y);
+// bitwise XOR: Sets each bit to 1 if only one of two bits is 1
+let z = 5 ^ 1;
+console.log(z);
 
-// // RegExp text search
-// let text = "Hi World"; // will print 3 because its the 3rd space
-// let a = text.search("World");
-// console.log(a);
+// RegExp text search
+let text = "Hi World"; // will print 3 because its the 3rd space
+let a = text.search("World");
+console.log(a);
 
-// // RegExp text replace
-// let text2 = "Hello World! Welcome to the best programming language, Python!"
-// let b = text2.replace("Python", "JavaScript");
-// console.log(b);
+// RegExp text replace
+let text2 = "Hello World! Welcome to the best programming language, Python!"
+let b = text2.replace("Python", "JavaScript");
+console.log(b);
 
-// // try/catch/throw/finally errors
-// function myFunction() {
-//     const message = document.getElementById("demo1");
-//     message.innerHTML = "";
-//     let x = document.getElementById("demo").value;
-//     try { 
-//       if(x == "")  throw ": is empty";
-//       if(isNaN(x)) throw ": is not a number";
-//       if (x = Number(x)) throw ": Good Job! Number was between 10 & 25.";
-//       if(x > 25)   throw ": is too high";
-//       if(x < 10)  throw ": is too low";
-//     }
-//     catch(err) {
-//       message.innerHTML = "Input " + err;
-//     }
-//     finally {
-//       document.getElementById("demo").value = "";
-//     }
-// }
+// try/catch/throw/finally errors
+function myFunction() {
+    const message = document.getElementById("demo1");
+    message.innerHTML = "";
+    let x = document.getElementById("demo").value;
+    try { 
+      if(x == "")  throw ": is empty";
+      if(isNaN(x)) throw ": is not a number";
+      if (x = Number(x)) throw ": Good Job! Number was between 10 & 25.";
+      if(x > 25)   throw ": is too high";
+      if(x < 10)  throw ": is too low";
+    }
+    catch(err) {
+      message.innerHTML = "Input " + err;
+    }
+    finally {
+      document.getElementById("demo").value = "";
+    }
+}
 
-//   // "this" keyword
-//   const person = {
-//       firstName : "Spencer",
-//       lastName : "Merrill",
-//       age : 25,
-//       fullName : function() {
-//           return "My full name is " + this.firstName + " " + this.lastName
-//           + ", and I am " + this.age;
-//       }
-//   };
-//   document.getElementById("name").innerHTML = person.fullName();
+  // "this" keyword
+  const person = {
+      firstName : "Spencer",
+      lastName : "Merrill",
+      age : 25,
+      fullName : function() {
+          return "My full name is " + this.firstName + " " + this.lastName
+          + ", and I am " + this.age;
+      }
+  };
+  document.getElementById("name").innerHTML = person.fullName();
 
-//   // arrow function 
-//   let arrowFunction = (a, b) => a + b;
-//   document.getElementById("number").innerHTML = "7 + 7 = " + arrowFunction(7, 7);
+  // arrow function 
+  let arrowFunction = (a, b) => a + b;
+  document.getElementById("number").innerHTML = "7 + 7 = " + arrowFunction(7, 7);
 
-//   // form validation
-//   function validateForm() {
-//     let x = document.forms["myForm"]["fname"].value;
-//     if (x == "") {
-//       alert("Name must be filled out");
-//       return false;
-//     }
-//   }
+  // form validation
+  function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
 
   // OOP object
   class Dog { 
